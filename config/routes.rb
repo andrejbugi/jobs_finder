3ec23650bc0c4acde6631f004a7f4daf64c8ca1d
andrejbugi <andrejbugi96@gmail.com>
2021-09-20 10:21:59 +0200

    change available options for user, add css to applications

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :jobs do
-    resources :job_applications
+    resources :job_applications, except: %i[edit update destroy]
   end
 
   root to: 'jobs#index'
